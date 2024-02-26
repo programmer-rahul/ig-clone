cec053e8f3ebd07bf0b22dc818e6b8543d6a82c5
Rahul <7678478061k@gmail.com>
2024-02-26 16:20:26 +0530

    added suggested users component

--- frontend/src/components/homepage/AllPostsContainer.tsx
+++ frontend/src/components/homepage/AllPostsContainer.tsx
@@ -3,11 +3,11 @@ import SinglePost from "./SinglePost";
 
 const AllPostsContainer = () => {
   return (
-    <div className="posts h-full text-stone-300 text-xs overflow-scroll overflow-x-auto p-4 md:w-ful lg:w-2/3 lg:order-2 w-full">
+    <div className="posts h-full text-stone-300 text-xs overflow-scroll overflow-x-auto p-4 md:w-ful lg:w-2/3 lg:order-2 w-full no-scrollbar">
       {/* stories  */}
       <div className="stories"></div>
 
-      <div className="all-posts border flex flex-col items-center gap-4 pt-8">
+      <div className="all-posts flex flex-col items-center gap-4 pt-8">
         {allPosts?.map((post, index) => {
           return <SinglePost key={index} post={post} />;
         })}
--- frontend/src/components/homepage/SuggestedUsers.tsx
+++ frontend/src/components/homepage/SuggestedUsers.tsx
@@ -1,8 +1,35 @@
+import Icon from "./Icon";
+
 const SuggestedUsers = () => {
   return (
-    <div className="suggested-users hidden lg:block bg-purple-600 lg:w-1/3 lg:order-3">
-      suggested users
+    <div className="suggested-users hidden lg:block lg:w-1/3 lg:order-3">
+      <div className="w-80 space-y-4 text-white py-8">
+        <div className="flex justify-between items-center">
+          <p className="text-stone-400 font-semibold">Suggested for you</p>
+          <p>See All</p>
+        </div>
+
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+      </div>
     </div>
   );
 };
 export default SuggestedUsers;
+
+const SuggestedUser = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="left gap-2 flex items-center font-semibold text-xl">
+        <Icon icon="defaultProfileIcon" size="10" />
+        <p>username</p>
+      </div>
+      <div className="right text-blue-400 font-semibold cursor-pointer">
+        Follow
+      </div>
+    </div>
+  );
+};
